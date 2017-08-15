a5e6d2459231c1d8f714ae0e8c953333df058d45
Erik Hofer <erik-hofer@mail.de>
2017-08-15 16:16:22 +0200

    Created project. Added generation of TypeScript model classes. Set up environment-dependent configuration.

---
+++ server/src/main/java/com/xinra/reviewcommunity/ReviewCommunityApplication.java
@@ -0,0 +1,12 @@
+package com.xinra.reviewcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewCommunityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReviewCommunityApplication.class, args);
+  }
+}
