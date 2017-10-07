ebc85ddb46cd52c1260ae0e654b335de3875bb38
Erik Hofer <erik-hofer@mail.de>
2017-10-07 16:09:07 +0200

    #15 Adapt SecurityConfiguration to restify the form login

--- server/src/main/java/com/xinra/reviewcommunity/rest/conf/SecurityConfiguration.java
+++ server/src/main/java/com/xinra/reviewcommunity/rest/conf/SecurityConfiguration.java
@@ -1,20 +1,46 @@
 package com.xinra.reviewcommunity.rest.conf;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableGlobalMethodSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+  
+  private @Autowired RestAuthenticationEntryPoint authenticationEntryPoint;
+  private @Autowired RestAuthenticationSuccessHandler authenticationSuccessHandler;
+  private @Autowired RestAuthenticationFailureHandler authenticationFailureHandler;
+  private @Autowired RestLogoutSuccessHandler logoutSuccessHandler;
   
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-        .authorizeRequests()
-            .antMatchers("/").permitAll();
+      .authorizeRequests()
+        .antMatchers("/").permitAll()
+      .and()
+        .formLogin()
+        .loginProcessingUrl("/session")
+        .successHandler(authenticationSuccessHandler)
+        .failureHandler(authenticationFailureHandler)
+      .and()
+        .logout()
+        .logoutRequestMatcher(new AntPathRequestMatcher("/session", HttpMethod.DELETE.name()))
+        .logoutSuccessHandler(logoutSuccessHandler)
+      .and()
+        .exceptionHandling()
+        .authenticationEntryPoint(authenticationEntryPoint);
     
+  }
+  
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
   }
   
 }
