27b146c4f22d4ca85cd536924040261c94486d3c
Erik Hofer <erik-hofer@mail.de>
2017-10-08 11:15:15 +0200

    #15 Add annotation @RequiresPermission to secure controller methods
    The annotation is used in place of @Secured with type-safety towards the Permission enum.

---
+++ server/src/main/java/com/xinra/reviewcommunity/rest/conf/PermissionVoter.java
@@ -0,0 +1,82 @@
+package com.xinra.reviewcommunity.rest.conf;
+
+import com.xinra.reviewcommunity.entity.Permission;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.vote.RoleVoter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+/**
+ * Enables the use of {@link Permission}s as authorities in Spring Security. The default
+ * {@link RoleVoter} only supports authorities prefixed with "ROLE_". That's how roles are
+ * distinguished from other authorities. This implementation doesn't rely on a prefix but checks if
+ * the authority is contained in the {@link Permission} enum.
+ * 
+ * <p>Voters are not used everywhere to check authorities. They are, however, used for method
+ * security. This class is required for {@link RequiresPermission} to work in place of
+ * {@link Secured}.
+ */
+public class PermissionVoter implements AccessDecisionVoter<Object> {
+  
+  /**
+   * This is a cache of string representations of all permissions. This way we can use
+   * {@link Set#contains(Object)} instead of {@link Permission#valueOf(String)} in
+   * {@link #supports(ConfigAttribute)}.
+   */
+  private final Set<String> permissions;
+  
+  /**
+   * Creates a PermissionVoter.
+   */
+  public PermissionVoter() {
+    permissions = new HashSet<>();
+    for (Permission permission : Permission.values()) {
+      permissions.add(permission.name());
+    }
+  }
+
+  @Override
+  public boolean supports(ConfigAttribute attribute) {
+    return permissions.contains(attribute.getAttribute());
+  }
+
+  @Override
+  public boolean supports(Class<?> clazz) {
+    return true;
+  }
+
+  /**
+   * @implSpec adapted from {@link RoleVoter}.
+   */
+  @Override
+  public int vote(Authentication authentication, Object object,
+      Collection<ConfigAttribute> attributes) {
+    
+    if (authentication == null) {
+      return ACCESS_DENIED;
+    }
+    int result = ACCESS_ABSTAIN;
+    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+    for (ConfigAttribute attribute : attributes) {
+      if (this.supports(attribute)) {
+        result = ACCESS_DENIED;
+
+        // Attempt to find a matching granted authority
+        for (GrantedAuthority authority : authorities) {
+          if (attribute.getAttribute().equals(authority.getAuthority())) {
+            return ACCESS_GRANTED;
+          }
+        }
+      }
+    }
+
+    return result;
+  }
+
+}
--- server/src/main/java/com/xinra/reviewcommunity/rest/conf/SecurityConfiguration.java
+++ server/src/main/java/com/xinra/reviewcommunity/rest/conf/SecurityConfiguration.java
@@ -8,13 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   
   private @Autowired RestAuthenticationEntryPoint authenticationEntryPoint;
