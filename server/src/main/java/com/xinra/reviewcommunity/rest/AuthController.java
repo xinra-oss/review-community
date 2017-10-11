83fb4cd00c73522d1a1927b645631b83071840db
Erik Hofer <erik-hofer@mail.de>
2017-10-11 10:58:39 +0200

    Remove unused imports

--- server/src/main/java/com/xinra/reviewcommunity/rest/AuthController.java
+++ server/src/main/java/com/xinra/reviewcommunity/rest/AuthController.java
@@ -1,7 +1,5 @@
 package com.xinra.reviewcommunity.rest;
 
-import com.xinra.reviewcommunity.auth.AccessRequires;
-import com.xinra.reviewcommunity.auth.Role;
 import com.xinra.reviewcommunity.rest.conf.MarketAgnostic;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.security.web.csrf.CsrfToken;
