83fc04badfcba62bb2f034fe48701320445a7045
Erik Hofer <erik-hofer@mail.de>
2017-10-18 11:47:05 +0200

    #22 Add exception handling to REST API

---
+++ server/src/main/java/com/xinra/reviewcommunity/rest/conf/ExceptionHandlerAdvice.java
@@ -0,0 +1,21 @@
+package com.xinra.reviewcommunity.rest.conf;
+
+import com.xinra.reviewcommunity.service.SerialNotFoundException;
+import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Global exception handler for the REST API (is applied to all controllers).
+ */
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+  
+  @ExceptionHandler(SerialNotFoundException.class)
+  public void handleNotFoundException(Exception ex, HttpServletResponse response)
+      throws IOException {
+    response.sendError(HttpServletResponse.SC_NOT_FOUND, ex.getMessage());
+  }
+
+}
