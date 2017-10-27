8fe53172332242127e812cb369f3c2c427ed62be
Erik Hofer <erik-hofer@mail.de>
2017-10-27 14:37:38 +0200

    #23 Add InitController

--- server/src/main/java/com/xinra/reviewcommunity/rest/FrontendUtil.java
+++ server/src/main/java/com/xinra/reviewcommunity/rest/FrontendUtil.java
@@ -1,5 +1,9 @@
 package com.xinra.reviewcommunity.rest;
 
+import com.xinra.nucleus.common.ApplicationContextProvider;
+import com.xinra.nucleus.service.DtoFactory;
+import com.xinra.reviewcommunity.dto.AuthenticatedUserDto;
+import com.xinra.reviewcommunity.dto.UserDto;
 import org.springframework.web.context.request.RequestContextHolder;
 
 public class FrontendUtil {
@@ -12,5 +16,19 @@ public class FrontendUtil {
   // TODO: move to nucleus-frontend
   public static boolean isRequest() {
     return RequestContextHolder.getRequestAttributes() != null;
+  }
+  
+  /**
+   * Creates a {@link UserDto} from an {@link AuthenticatedUserDto}. This is necessary if the
+   * authenticated user should be returned by a REST endpoint. Even if the declared type is
+   * {@link UserDto}, all properties would be marshaled. Thus a new DTO is created that only
+   * contains the data that may be accessed by the client.
+   */
+  public static UserDto trimAuthenticatedUser(AuthenticatedUserDto user) {
+    UserDto userDto = ApplicationContextProvider.getApplicationContext()
+        .getBean(DtoFactory.class).createDto(UserDto.class);
+    userDto.setLevel(user.getLevel());
+    userDto.setName(user.getName());
+    return userDto;
   }
 }
