c8d502252aace251087ee7d892fabb21f918d29f
Erik Hofer <erik-hofer@mail.de>
2017-11-05 13:34:24 +0100

    #38 Add test for AuthController

---
+++ server/src/test/java/com/xinra/reviewcommunity/rest/TestAuthController.java
@@ -0,0 +1,29 @@
+package com.xinra.reviewcommunity.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+public class TestAuthController {
+
+  private @Autowired MockMvc mvc;
+
+  @Test
+  public void getCsrfToken() throws Exception {
+    mvc.perform(get("/api/csrf-token"))
+        .andExpect(status().isOk());
+  }
+
+}
