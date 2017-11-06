0d9e7702ec077c22f6ff559b2a60f3a1521a81ad
Erik Hofer <erik-hofer@mail.de>
2017-11-06 16:03:50 +0100

    #38 Make InitController return empty list of permissions if not authenticated
    (instead of null)

--- server/src/main/java/com/xinra/reviewcommunity/rest/InitController.java
+++ server/src/main/java/com/xinra/reviewcommunity/rest/InitController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+
 @RestController
 public class InitController extends AbstractController {
   
@@ -32,7 +34,7 @@ public class InitController extends AbstractController {
     initDto.setCsrfToken(authController.getCsrfToken(request));
     initDto.setAuthenticatedUser(authenticatedUser);
     initDto.setPermissions(contextHolder.get().getAuthenticatedUser()
-        .map(AuthenticatedUserDto::getPermissions).orElse(null));
+        .map(AuthenticatedUserDto::getPermissions).orElse(Collections.emptySet()));
     initDto.setMarkets(marketController.getList());
     initDto.setCategoryTree(categoryController.getList());
     
