8add2914b82fb383b2115afcd069c7fbcdc4b021
Erik Hofer <erik-hofer@mail.de>
2017-11-08 12:27:38 +0100

    #39 Make controller tests use proper mock authentication

--- server/src/test/java/com/xinra/reviewcommunity/rest/TestProductController.java
+++ server/src/test/java/com/xinra/reviewcommunity/rest/TestProductController.java
@@ -7,14 +7,15 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.xinra.reviewcommunity.SampleContentGenerator;
 import com.xinra.reviewcommunity.entity.Product;
 import com.xinra.reviewcommunity.repo.ProductRepository;
+import com.xinra.reviewcommunity.service.AuthenticationProviderImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.TestingAuthenticationToken;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,6 +30,7 @@ public class TestProductController {
 
   private @Autowired MockMvc mvc;
   private @Autowired ProductRepository<Product> productRepo;
+  private @Autowired SampleContentGenerator sample;
 
   @Test
   public void createProduct() throws Exception {
@@ -42,7 +44,7 @@ public class TestProductController {
 
     // creating a product should work
     mvc.perform(post("/de/api/product").contentType("application/json").content(content)
-        .with(authentication(new TestingAuthenticationToken("jon", "snow", "CREATE_PRODUCT")))
+        .with(authentication(AuthenticationProviderImpl.getAuthentication(sample.user)))
         .with(csrf()))
         .andExpect(status().isOk());
 
