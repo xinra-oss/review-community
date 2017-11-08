5a89596cbce9fce8a332dfe34c3fe21d721db5e4
Toby Moeller <toby.moeller@gmx.de>
2017-11-08 17:44:38 +0100

    #43 Create CategoryListActivity

---
+++ android/src/main/java/com/xinra/reviewcommunity/android/CategoryListActivity.java
@@ -0,0 +1,13 @@
+package com.xinra.reviewcommunity.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CategoryListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_list);
+    }
+}
