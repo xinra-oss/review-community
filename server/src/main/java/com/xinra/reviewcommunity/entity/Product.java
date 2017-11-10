000050dbac95e4adbe90d07c2f40489f4fc4c692
Erik Hofer <erik-hofer@mail.de>
2017-11-10 10:47:11 +0100

    #40 Configure indexing of Product entity

--- server/src/main/java/com/xinra/reviewcommunity/entity/Product.java
+++ server/src/main/java/com/xinra/reviewcommunity/entity/Product.java
@@ -5,24 +5,41 @@ import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.Index;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.IndexedEmbedded;
+import org.hibernate.search.annotations.Store;
 
 @Entity
 @Getter
 @Setter
+@Indexed
 public class Product extends SerialEntity {
 
+  @Field(store = Store.YES)
   private String name;
+  
+  @Field(store = Store.YES)
   private String description;
 
+  @IndexedEmbedded
   @ManyToOne(optional = false)
   private @NonNull Category category;
   
   @ManyToOne
+  @IndexedEmbedded
   private Brand brand;
   
   // cache
+  
+  @Field(index = Index.NO, store = Store.YES)
   private double avgRating;
+  
+  @Field(index = Index.NO, store = Store.YES)
   private int numRatings;
+  
+  @Field(index = Index.NO, store = Store.YES)
   private double score;
 
 }
