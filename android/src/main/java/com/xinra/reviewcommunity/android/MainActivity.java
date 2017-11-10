da9533bd3f0abd393acd1932387a7602a6254130
Toby Moeller <toby.moeller@gmx.de>
2017-11-10 08:51:34 +0100

    #43 Implement Category Button on MainActivity + onclick

--- android/src/main/java/com/xinra/reviewcommunity/android/MainActivity.java
+++ android/src/main/java/com/xinra/reviewcommunity/android/MainActivity.java
@@ -1,6 +1,10 @@
 package com.xinra.reviewcommunity.android;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends BaseActivity {
 
@@ -8,6 +12,13 @@ public class MainActivity extends BaseActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Button addCategoryListBtn = findViewById(R.id.mainCategoryListBtn);
+    addCategoryListBtn.setOnClickListener(view -> {
+      Intent categoriesIntent = new Intent(getApplicationContext(), CategoryListActivity.class);
+      startActivity(categoriesIntent);
+    });
   }
+
   
 }
