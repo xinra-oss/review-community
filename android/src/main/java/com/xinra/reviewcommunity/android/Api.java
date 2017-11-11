ce34b7ffbb216be6afb4d523df4876f3a077b0d4
Erik Hofer <erik-hofer@mail.de>
2017-11-11 23:27:33 +0100

    #45 Fix Api

--- android/src/main/java/com/xinra/reviewcommunity/android/Api.java
+++ android/src/main/java/com/xinra/reviewcommunity/android/Api.java
@@ -3,31 +3,19 @@ package com.xinra.reviewcommunity.android;
 import com.google.common.collect.ImmutableMap;
 import com.xinra.reviewcommunity.shared.dto.CsrfTokenDto;
 import com.xinra.reviewcommunity.shared.dto.InitDto;
-import com.xinra.reviewcommunity.shared.dto.MarketDto;
 import com.xinra.reviewcommunity.shared.dto.ProductDto;
 import com.xinra.reviewcommunity.shared.dto.RegistrationDto;
 import com.xinra.reviewcommunity.shared.dto.SuccessfulAuthenticationDto;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 import io.reactivex.Completable;
 import io.reactivex.Single;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.schedulers.Schedulers;
 
 public class Api extends AbstractApi {
 
@@ -58,8 +46,8 @@ public class Api extends AbstractApi {
     return withoutResponse("/user", HttpMethod.POST, registration, true, null);
   }
 
-  public Single<List<ProductDto>> search(String query) {
-    return withResponse("/product", HttpMethod.GET, ProductDto[].class, null, false, ImmutableMap.of("q", query))
+  public Single<List<ProductDto>> getProductList(String query) {
+    return withResponse("/product?q={query}", HttpMethod.GET, ProductDto[].class, null, false, ImmutableMap.of("query", query))
         .map(Arrays::asList);
   }
 }
--- android/src/main/java/com/xinra/reviewcommunity/android/SearchActivity.java
+++ android/src/main/java/com/xinra/reviewcommunity/android/SearchActivity.java
@@ -7,8 +7,8 @@ import android.support.v7.widget.SearchView;
 
 public class SearchActivity extends AbstractActivity {
 
-  // Current behaviour: if you use the search widget in the action bar you get here. If you then
-  // change the query using the search widget embedded in this activity, you stay in this activity
+  // Current behaviour: if you use the getProductList widget in the action bar you get here. If you then
+  // change the query using the getProductList widget embedded in this activity, you stay in this activity
   // and fire a new intent. If you then press the back button, this activity is finished (you don't
   // go back to the original query). This is due to android:launchMode="singleTop".
   // Be careful when trying to change this. The automatic creation of intents is somehow
@@ -17,7 +17,7 @@ public class SearchActivity extends AbstractActivity {
   // approach would be to handle intent creation ourselves.
 
   // This also means that currently, sometimes multiple requests are made to the REST API when
-  // using the action bar search. This could be fixed by omitting onNewIntent() and using a
+  // using the action bar getProductList. This could be fixed by omitting onNewIntent() and using a
   // custom onQueryTextListener for the embedded widget.
 
   private SearchView searchWidget;
@@ -52,6 +52,6 @@ public class SearchActivity extends AbstractActivity {
 
   private void performSearch(String query) {
     // Technically it may be possible to get here before initialization. We'll ignore this for now.
-    getApi().search(query).subscribe(resultsView::setContent, this::handleError);
+    getApi().getProductList(query).subscribe(resultsView::setContent, this::handleError);
   }
 }
