42535f49e8e7d162603df57be7af5d6f6e870ded
Erik Hofer <erik-hofer@mail.de>
2017-11-15 22:13:51 +0100

    #50 Create SelectCategoryActivity

---
+++ android/src/main/java/com/xinra/reviewcommunity/android/SelectCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.xinra.reviewcommunity.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectCategoryActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_select_category);
+  }
+}
